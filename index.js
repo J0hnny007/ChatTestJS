b33f6de1f2952daa4fa3835d67d78f49f9ebcc7d
J0hnny007 <33027466+J0hnny007@users.noreply.github.com>
2021-04-27 09:57:10 +0200

    test 2

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', function(req, res) {
     res.render('index.ejs');
 });
@@ -21,4 +41,8 @@ io.sockets.on('connection', function(socket) {
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
     });
 
+});
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
 });
