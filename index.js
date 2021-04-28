92b67b75f00186b8362446fdad44a6e5a0a748eb
J0hnny007 <33027466+J0hnny007@users.noreply.github.com>
2021-04-28 21:21:23 +0200

    reworked entire example from ne tutoriel

--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+var port = 3000;
+var express = require('express')
+var ejs = require('ejs')
+var layouts = require('express-ejs-layouts')
+var app = express();
 
-// Add headers
-app.use(function (req, res, next) {
+app.set('view engine', 'ejs');
+app.use(layouts);
+app.use(express.static('public'));
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+//require homeController.js for our renderIndex() function
+var homeController = require('./controllers/homeController');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//add the route for our chat
+app.get('/', homeController.renderIndex);
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+var server = app.listen(port, () => {
+  console.log(`Sever listening on port ${port}`);
 });
 
-app.get('/', function(req, res) {
-    res.render('index.ejs');
-});
-
-io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
-        socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-    });
-
-    socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-    })
-
-    socket.on('chat_message', function(message) {
-        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
-    });
-
-});
-
-const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
-});
+var io = require('socket.io')(server);
+var socketController = require('./controllers/socketController.js')(io);
